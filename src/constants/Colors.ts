5ad4a7cbaf06a64474ae8b04fb2e1cc9e97186cc
markgomer <aureliojuniorcmrj@hotmail.com>
2025-04-04 14:15:45 -0300

    Delete bloat and move files to src/

---
+++ src/constants/Colors.ts
@@ -0,0 +1,33 @@
+const tintColorLight = '#2f95dc';
+const tintColorDark = '#fff';
+
+export default {
+    light: {
+        text: '#000',
+        background: '#fff',
+        tint: tintColorLight,
+        tabIconDefault: '#ccc',
+        tabIconSelected: tintColorLight,
+    },
+    dark: {
+        text: '#fff',
+        background: '#000',
+        tint: tintColorDark,
+        tabIconDefault: '#ccc',
+        tabIconSelected: tintColorDark,
+    },
+    gray: {
+        100: "#F2F2F2",
+        200: "#D9D9D9",
+        300: "#808080",
+        400: "#333333",
+        500: "#262626",
+        600: "#1A1A1A",
+        700: "#0D0D0D",
+    },
+    purple: "#8284FA",
+    purple_dark: "#5E60CE",
+    blue: "4EA8DE",
+    blue_dark: "1E6F9F",
+    danger: "#E25858",
+};
